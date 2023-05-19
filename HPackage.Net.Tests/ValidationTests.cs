449c42914dc58fc35f4a87c2b941ad745f9b1b8f
Sean Dempsey <dempsey.sean@outlook.com>
2023-05-18 21:19:37 -0700

    Proper testing setup

---
+++ HPackage.Net.Tests/ValidationTests.cs
@@ -0,0 +1,11 @@
+namespace HPackage.Net.Tests
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void Bad()
+        {
+            //Assert.Fail("Bad");
+        }
+    }
+}
