7c0b948bbdab7d95989e80d3923929a222e5b3e0
Sean Dempsey <dempsey.sean@outlook.com>
2023-05-26 10:49:14 -0700

    Add postprocessing for StringVersion

---
+++ HPackage.Net.Tests/VersionProcessingTests.cs
@@ -0,0 +1,99 @@
+﻿namespace HPackage.Net.Tests
+{
+    public class VersionProcessingTests
+    {
+        [Fact]
+        public void EmptyReferenceVersionThrows()
+        {
+            Action action = () => new ReferenceVersion().GetReferenceDef();
+            action.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void ReferenceVersionWithReferenceDefReturnsReferenceDef()
+        {
+            ReferenceVersion version = new ReferenceDef
+            {
+                AlternateInstallName = "A",
+                FileType = FileType.Dll,
+                Ref = new Reference
+                {
+                    UseLatestPublished = true,
+                }
+            };
+            version.GetReferenceDef().Should().BeSameAs(version.ReferenceDef);
+        }
+
+        [Fact]
+        public void UriVersionReturnsLinkDef()
+        {
+            ReferenceVersion version = "https://example.com/someFile.dll";
+            ReferenceDef expected = new ReferenceDef
+            {
+                Ref = new Reference
+                {
+                    Link = new Uri("https://example.com/someFile.dll")
+                }
+            };
+            version.GetReferenceDef().Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ModlinksReturnsModlinksReference()
+        {
+            ReferenceVersion version = "@modlinks";
+            ReferenceDef expected = new ReferenceDef
+            {
+                Ref = new Reference
+                {
+                    UseLatestPublished = true,
+                }
+            };
+            version.GetReferenceDef().Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ModlinksVersionFormatReturnsModlinksReference()
+        {
+            ReferenceVersion version = "1.0.1.0";
+            ReferenceDef expected = new ReferenceDef
+            {
+                Ref = new Reference
+                {
+                    Version = "1.0.1.0"
+                }
+            };
+            version.GetReferenceDef().Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void LatestReturnsGitReference()
+        {
+            ReferenceVersion version = "@latest";
+            ReferenceDef expected = new ReferenceDef
+            {
+                Ref = new Reference
+                {
+                    UseLatestRelease = true,
+                }
+            };
+            version.GetReferenceDef().Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData("v1.0.1.0")]
+        [InlineData("adasdfasdf")]
+        public void AnythingElseReturnsGitReference(string versionString)
+        {
+            ReferenceVersion version = versionString;
+            ReferenceDef expected = new ReferenceDef
+            {
+                Ref = new Reference
+                {
+                    Tag = versionString
+                }
+            };
+            version.GetReferenceDef().Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ HPackage.Net/ReferenceVersion.PostProcessing.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HPackage.Net
+{
+    public partial struct ReferenceVersion
+    {
+        private static readonly Regex modlinksVersionRegex = new(@"^(\d+\.){3}\d+$");
+
+        /// <summary>
+        /// Fetches or constructs the final ReferenceDef representing this reference, processing the string version if needed.
+        /// The created ReferenceDef may not be valid for serialization as modlinks versions and Git tag names are ambiguous.
+        /// </summary>
+        /// <returns>The ReferenceDef represented by this ReferenceVersion.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when both ReferenceDef and String versions are null.</exception>
+        public ReferenceDef GetReferenceDef()
+        {
+            if (this.ReferenceDef != null)
+            {
+                return this.ReferenceDef;
+            }
+            if (this.String == null)
+            {
+                throw new InvalidOperationException("ReferenceDef and String are both null; one is needed to construct the correct ReferenceDef.");
+            }
+            string version = this.String;
+            if (Uri.TryCreate(version, UriKind.Absolute, out Uri uriVersion))
+            {
+                return new ReferenceDef
+                {
+                    Ref = new Reference
+                    {
+                        Link = uriVersion,
+                    }
+                };
+            }
+            else if (version == "@modlinks")
+            {
+                return new ReferenceDef
+                {
+                    Ref = new Reference
+                    {
+                        UseLatestPublished = true,
+                    }
+                };
+            }
+            else if (version == "@latest")
+            {
+                return new ReferenceDef
+                {
+                    Ref = new Reference
+                    {
+                        UseLatestRelease = true,
+                    }
+                };
+            }
+            else if (modlinksVersionRegex.IsMatch(version))
+            {
+                return new ReferenceDef
+                {
+                    Ref = new Reference
+                    {
+                        Version = version,
+                    }
+                };
+            }
+            else
+            {
+                return new ReferenceDef
+                {
+                    Ref = new Reference
+                    {
+                        Tag = version,
+                    }
+                };
+            }
+        }
+    }
+}
